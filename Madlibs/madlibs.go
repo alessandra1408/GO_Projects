fd9a7fd6dc83f19bb406eda41d09bf5a03fc589e
alessandra1408 <alessandraoitnb@gmail.com>
2022-09-21 23:39:14 -0300

    Add another way to concatenate

--- Madlibs/madlibs.go
+++ Madlibs/madlibs.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var youtube string
 	fmt.Println("Insira o nome do seu canal: ")
-	fmt.Scanln(&youtube)
-	fmt.Println("Subscribe to ", youtube)
+	var youtube string
 
+	fmt.Scanln(&youtube)
+	fmt.Println("\nSubscribe to ", youtube)
+	fmt.Printf("\nSubscribe to %s ", youtube)
 }
