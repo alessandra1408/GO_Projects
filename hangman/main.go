5591edc30a49375f79d581362020288725a66c78
alessandra1408 <alessandraoitnb@gmail.com>
2022-10-27 15:39:03 -0300

    Add a welcomePlayer funciont

--- hangman/main.go
+++ hangman/main.go
@@ -3,23 +3,12 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 )
 
-func selectLanguage() {
-	//usar switch
-
-}
-
-func getValidWords() []string {
-
-	//se for array pt-br
-	//fazer um loop for para:
-	//todas as posições do array menores que 3 serem excluídas
-	//gerar um novo array com essas novas palavras
-
+func selectLanguage() []string {
 	var arrayInput = []string{}
-	var validWords = []string{}
 	languageOption := 0
 
 	fmt.Println("** Language Selection **")
@@ -34,6 +23,19 @@ func getValidWords() []string {
 		arrayInput = wordsPT
 	}
 
+	return arrayInput
+
+}
+
+func getValidWords(arrayInput []string) []string {
+
+	//se for array pt-br
+	//fazer um loop for para:
+	//todas as posições do array menores que 3 serem excluídas
+	//gerar um novo array com essas novas palavras
+
+	var validWords []string
+
 	for i := 0; i < len(arrayInput); i++ {
 		if len(arrayInput[i]) >= 3 {
 			validWords = append(validWords, arrayInput[i])
@@ -41,17 +43,62 @@ func getValidWords() []string {
 	}
 
 	return validWords
+
+}
+
+func welcomePlayer() {
+	asciiArt := `
+	 _   _    _    _   _  ____ __  __    _    _   _
+	| | | |  / \  | \ | |/ ___|  \/  |  / \  | \ | |
+	| |_| | / _ \ |  \| | |  _| |\/| | / _ \ |  \| |
+	|  _  |/ ___ \| |\  | |_| | |  | |/ ___ \| |\  |
+	|_| |_/_/   \_\_| \_|\____|_|  |_/_/   \_\_| \_|
+	`
+	fmt.Println(asciiArt)
 }
 
 func hangman(validArray []string) {
 
+	var usedLetters = []string{}
+
 	rand.Seed(time.Now().UTC().UnixNano())
 	randWord := rand.Intn(len(validArray))
-	fmt.Println(validArray[randWord])
+	generatedWord := validArray[randWord]
+
+	var letters string
+	fmt.Println("Try to guess de word!")
+
+	for vida := 5; vida > 0; vida-- {
+		fmt.Scanln(&letters)
+
+		if len(letters) != 1 {
+			fmt.Println("You can only use one letter at a time! You lsot a life.")
+
+		} else {
+
+			for i := 0; i != len(generatedWord); i++ {
+
+				if string(letters) == string(generatedWord[i]) {
+					fmt.Printf("a letra %v tem na palavra!\n", letters)
+				}
+
+				fmt.Printf("_ ")
+			}
+
+			fmt.Println()
+			fmt.Println(generatedWord)
+
+			usedLetters = append(usedLetters, letters)
+			usedLettersConcat := strings.Join(usedLetters, " ")
+			fmt.Printf("You have used these letters: %v\n", usedLettersConcat)
+		}
+	}
+
 }
 
 func main() {
-	selectLanguage()
-	validArray := getValidWords()
+	welcomePlayer()
+	arrayInput := selectLanguage()
+	validArray := getValidWords(arrayInput)
 	hangman(validArray)
 }
